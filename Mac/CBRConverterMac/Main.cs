3a03b0e839cf944161e19d8bb8b6928c2e345b9e
C.W. Betts <computers57@hotmail.com>
2018-10-20 02:42:54 -0600

    Initial Mac version work.

---
+++ Mac/CBRConverterMac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace CBRConverterMac
+{
+    static class MainClass
+    {
+        static void Main(string[] args)
+        {
+            NSApplication.Init();
+            NSApplication.Main(args);
+        }
+    }
+}
